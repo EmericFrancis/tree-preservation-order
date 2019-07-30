cf1e6dec2cc3e5ecd1d863c53a421c5f8548af7e
Emeric Francis <emeric.francis@landregistry.gov.uk>
2019-07-30 09:50:42 +0100

    adding google maps javascript files import to scripts.html also  adding a few screens of the add flow

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,17 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.post('/here-now-answer', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let herenow = req.session.data['location']
+
+  if (herenow === 'here-yes') {
+    res.redirect('add-1-yes')
+  } else {
+    res.redirect('add-1-no')
+  }
+})
